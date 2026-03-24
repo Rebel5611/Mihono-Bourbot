51f43985b82f961b2063384ec7cfb3aeaf41e6e2
Zane Jacobi <117382036+Rebel5611@users.noreply.github.com>
2026-03-24 14:19:16 -0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,23 @@
+import nextcord
+from nextcord.ext import commands
+import os
+from apikeys import *
+
+intents = nextcord.Intents.default()
+intents.message_content = True
+
+client = commands.Bot(command_prefix='/', intents=intents)
+
+@client.event
+async def on_ready():
+    print("Ready")
+
+initial_extensions = []
+for filename in os.listdir('cogs'):
+    if filename.endswith('.py'):
+        initial_extensions.append("cogs." + filename[:-3])
+
+if __name__ == '__main__':
+    client.load_extensions(initial_extensions)
+
+client.run(BOTTOKEN)
